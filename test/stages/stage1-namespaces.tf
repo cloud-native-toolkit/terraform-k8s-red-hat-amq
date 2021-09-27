acd9ea8b0c12deb3055d507e10acef7eca90176a
Yogendra Srivastava <ysrivastava@in.ibm.com>
2021-09-27 16:27:00 +0530

    updated test
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- test/stages/stage1-namespaces.tf
+++ test/stages/stage1-namespaces.tf
@@ -1,8 +1,7 @@
 module "dev_tools_namespace" {
   source = "github.com/ibm-garage-cloud/terraform-k8s-namespace.git"
 
-  cluster_type             = module.dev_cluster.type_code
-  cluster_config_file_path = module.dev_cluster.config_file_path
-  tls_secret_name          = module.dev_cluster.tls_secret_name
+  
+  cluster_config_file_path = module.dev_cluster.config_file_path  
   name                     = var.tools_namespace
 }
